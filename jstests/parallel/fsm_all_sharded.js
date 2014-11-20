74e31a380e7f00fbc57337e37e2ccd9529ede054
David Percy <david.percy@mongodb.com>
2014-11-20 16:45:57 -0500

    agg workloads

--- jstests/parallel/fsm_all_sharded.js
+++ jstests/parallel/fsm_all_sharded.js
@@ -7,11 +7,14 @@ var dir = 'jstests/parallel/fsm_workloads';
 var blacklist = [
 
     // Disabled due to known bugs
+    'agg_match.js', // SERVER-3645 .count() can be wrong on sharded collections
     'drop_database.js', // SERVER-16285
     'map_reduce_merge_nonatomic.js', // SERVER-16262
     'map_reduce_reduce_nonatomic.js', // SERVER-16262
 
     // Disabled due to MongoDB restrictions and/or workload restrictions
+    'agg_group_external.js', // uses >100MB of data, and is flaky
+    'agg_sort_external.js', // uses >100MB of data, and is flaky
     'indexed_insert_eval.js', // eval doesn't work with sharded collections
     'indexed_insert_eval_nolock.js', // eval doesn't work with sharded collections
     'remove_single_document.js', // our .remove(query, {justOne: true}) calls lack shard keys
